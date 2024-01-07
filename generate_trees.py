e2996231116e5c0379d11f1f2aa9c84e7de7f43f
Willu12 <boryskurdek@gmail.com>
2024-01-07 18:06:18 +0100

    fixed function

--- generate_trees.py
+++ generate_trees.py
@@ -2,7 +2,7 @@ import random
 import sys
 
 from igraph import Graph
-
+import math
 import convert_representation
 from convert_representation import convert_parenthesized_to_tree
 
@@ -53,10 +53,13 @@ def generate_tree(n: int) -> Graph:
     elif root_paired == 1 or root_paired == 2:
         root_unpaired = random.randint(0,n)
 
+    print(root_paired,root_unpaired)
     n -= root_unpaired
     print(n)
-    child_amount = root_paired * 2 + root_unpaired
+    child_amount = root_paired + root_unpaired
     pair_indexes = random.sample(range(0, child_amount), root_paired)
+    pair_indexes.sort()
+    print(pair_indexes)
 
     process_pairs(g,pair_indexes,stack,root_paired,root_unpaired,0)
 
@@ -69,16 +72,22 @@ def generate_tree(n: int) -> Graph:
         # powtroz poprzednia opearacje
         if n <= 0:
             continue
-        node_paired = random.randint(0, min(3, n/2))
+        if n == 1:
+            node_paired = 0
+        else:
+            node_paired = random.randint(0, min(3, math.floor(n/2)))
         node_unpaired = 0
         if node_paired == 0 or node_paired == 1:
             node_unpaired = random.randint(0, n - node_paired * 2)
 
-        child_amount = node_paired * 2 + node_unpaired
-        n -= child_amount
+        child_amount = node_paired + node_unpaired
+        n -= node_paired * 2 + node_unpaired
         print(n)
-        pair_indexes = random.sample(range(1, child_amount + 1), node_paired)
+        pair_indexes = random.sample(range(0, child_amount), node_paired)
+        pair_indexes.sort()
         process_pairs(g,pair_indexes,stack,node_paired,node_unpaired,v.index)
+        if len(stack) == 0 and n > 0:
+            v["unpaired_count_0"] = n
 
     return g
 
